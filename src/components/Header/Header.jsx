fe771e73e9dbc1e76f4555ecc28950b4f042f083
pinki <Pnegi061997@gmail.comgit config --global user.email Pnegi061997@gmail.com>
2024-09-02 11:55:39 +0530

    initial commit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className='header'>
+    <div className="header-contents">
+      <h2>Order your favourite food from here</h2>
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vero accusantium, fugiat, obcaecati modi impedit aperiam quam doloremque libero dignissimos ea natus! Nemo autem rem expedita tempore consequuntur provident officia, quas velit, inventore saepe eligendi tenetur veritatis accusantium blanditiis sint ducimus!</p>
+    <button>View Menu</button>
+    </div>
+      
+    </div>
+  )
+}
+
+export default Header
